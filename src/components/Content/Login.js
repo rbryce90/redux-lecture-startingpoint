db0806a618d0812b1eadcea0dd8b08a5c791bfd7
Richard Bond <richardbond@Richards-MacBook-Pro.local>
2019-02-04 11:01:57 -0700

    without redux

--- src/components/Content/Login.js
+++ src/components/Content/Login.js
@@ -8,13 +8,24 @@ export default class Login extends Component {
     };
   }
 
+  updateInput(event) {}
+
   render() {
+    console.log(this.state.name);
     return (
       <div>
         <h1>Login!</h1>
         {/* save the onchange value to localstate and the submitted will value will get passed back to the parent */}
-        <input />
-        <button>Submit</button>
+        <input
+          value={this.state.name}
+          onChange={(event) =>
+            this.setState({
+              name: event.target.value
+            })
+          }
+        />
+        <button
+          onClick={ () => this.props.login(this.state.name) }>Submit</button>
       </div>
     );
   }
--- src/components/Header/UserSection.js
+++ src/components/Header/UserSection.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import UserName from './UserName'
 
 export default class UserSection extends Component {
   render() {
     return (
       <div className="user-section-container">
-        <span>User:</span>
-        {/* UserName Component */}
+       <span>User:</span>
+       <UserName user={this.props.user}/>
       </div>
     );
   }
